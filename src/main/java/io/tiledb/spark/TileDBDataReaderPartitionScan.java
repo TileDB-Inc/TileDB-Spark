01357f646e84bc0a3a2b0e5fac60a66205388e12
Seth Shelnutt <seth@tiledb.io>
2019-08-22 17:54:33 -0400

    Improve incomplete query support
    Check for buffer reallocation allowment and check available memory to
    double buffers.
    
    Also keep track of native buffers so we can purge them out before
    doubling

--- src/main/java/io/tiledb/spark/TileDBDataReaderPartitionScan.java
+++ src/main/java/io/tiledb/spark/TileDBDataReaderPartitionScan.java
@@ -1,12 +1,19 @@
 package io.tiledb.spark;
 
+import static io.tiledb.java.api.Constants.TILEDB_COORDS;
 import static io.tiledb.java.api.Datatype.TILEDB_UINT64;
+import static io.tiledb.java.api.QueryStatus.TILEDB_COMPLETED;
+import static io.tiledb.java.api.QueryStatus.TILEDB_INCOMPLETE;
+import static io.tiledb.java.api.QueryStatus.TILEDB_UNINITIALIZED;
 
 import io.tiledb.java.api.*;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.spark.sql.execution.vectorized.OnHeapColumnVector;
 import org.apache.spark.sql.sources.EqualNullSafe;
@@ -21,6 +28,7 @@ import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.vectorized.ColumnarBatch;
+import oshi.hardware.HardwareAbstractionLayer;
 import scala.annotation.meta.field;
 
 public class TileDBDataReaderPartitionScan implements InputPartitionReader<ColumnarBatch> {
@@ -29,6 +37,9 @@ public class TileDBDataReaderPartitionScan implements InputPartitionReader<Colum
 
   // Filter pushdown to this partition
   private final Filter[] pushedFilters;
+
+  // HAL for getting memory details about doubling buffers
+  private final HardwareAbstractionLayer hardwareAbstractionLayer;
 
   // read buffer size
   private long read_query_buffer_size;
@@ -54,38 +65,81 @@ public class TileDBDataReaderPartitionScan implements InputPartitionReader<Colum
   // Spark batch column vectors
   private OnHeapColumnVector[] resultVectors;
 
-  // signals columnar batch query termination
-  private boolean hasNext;
+  // Query status
+  private QueryStatus queryStatus;
+
+  /**
+   * List of NativeArray buffers used in the query object. This is indexed based on columnHandles
+   * indexing (aka query field indexes)
+   */
+  private final ArrayList<Pair<NativeArray, NativeArray>> queryBuffers;
 
   public TileDBDataReaderPartitionScan(
       URI uri, TileDBReadSchema schema, TileDBDataSourceOptions options, Filter[] pushedFilters) {
     this.arrayURI = uri;
     this.sparkSchema = schema.getSparkSchema();
     this.options = options;
-    this.hasNext = false;
+    this.queryStatus = TILEDB_UNINITIALIZED;
     this.pushedFilters = pushedFilters;
 
     this.read_query_buffer_size = options.getReadBufferSizes();
+
+    oshi.SystemInfo systemInfo = new oshi.SystemInfo();
+
+    this.hardwareAbstractionLayer = systemInfo.getHardware();
+    this.queryBuffers =
+        new ArrayList<>(Collections.nCopies(schema.getSparkSchema().fields().length, null));
+
+    try {
+      // init query
+      this.initQuery();
+    } catch (TileDBError tileDBError) {
+      tileDBError.printStackTrace();
+    }
   }
 
   @Override
   public boolean next() {
-    try {
+    try (Domain domain = arraySchema.getDomain()) {
       // first submission initialize the query and see if we can fast fail;
       if (query == null) {
-        hasNext = initQuery();
+        initQuery();
       }
-      if (!hasNext) {
+
+      // If the query was completed, and we have exhausted all records then we should close the
+      // cursor
+      if (queryStatus == TILEDB_COMPLETED) {
         return false;
       }
-      boolean prevNext = hasNext;
-      query.submit();
-      // TODO handle zero result realloc case
-      hasNext = query.getQueryStatus() == QueryStatus.TILEDB_INCOMPLETE;
-      return prevNext;
+
+      do {
+        query.submit();
+
+        queryStatus = query.getQueryStatus();
+
+        // Compute the number of cells (records) that were returned by the query.
+        HashMap<String, Pair<Long, Long>> queryResultBufferElements = query.resultBufferElements();
+        long currentNumRecords =
+            queryResultBufferElements.get(TILEDB_COORDS).getSecond() / domain.getNDim();
+
+        // Increase the buffer allocation and resubmit if necessary.
+        if (queryStatus == TILEDB_INCOMPLETE && currentNumRecords == 0) { // VERY IMPORTANT!!
+          if (options.getAllowReadBufferReallocation()) {
+            reallocateQueryBuffers();
+          } else {
+            throw new RuntimeException(
+                "Incomplete query with no more records means the buffers are too small but allow_read_buffer_realloc is set to false!");
+          }
+        } else if (currentNumRecords > 0) {
+          // Break out of resubmit loop as we have some results.
+          return true;
+        }
+      } while (queryStatus == TILEDB_INCOMPLETE);
     } catch (TileDBError err) {
       throw new RuntimeException(err.getMessage());
     }
+
+    return true;
   }
 
   @Override
@@ -123,6 +177,10 @@ public class TileDBDataReaderPartitionScan implements InputPartitionReader<Colum
     if (resultBatch != null) {
       resultBatch.close();
     }
+
+    closeQueryNativeArrays();
+    queryBuffers.clear();
+
     if (query != null) {
       query.close();
     }
@@ -177,37 +235,126 @@ public class TileDBDataReaderPartitionScan implements InputPartitionReader<Colum
       // set query read layout
       setOptionQueryLayout(options.getArrayLayout());
 
-      // loop over all attributes and set the query buffers based on the result size estimate
+      allocateQuerybuffers(this.read_query_buffer_size);
+    }
+    // est that there are resuts, so perform a read for this partition
+    return true;
+  }
+
+  /**
+   * Function to calculate the bytes read based on the buffer sizes
+   *
+   * @return byte in current buffers
+   */
+  private long calculateNativeArrayByteSizes() {
+    long totalBufferSizes = 0;
+    long bufferCount = 0;
+    long largestSingleBuffer = 0;
+    for (Pair<NativeArray, NativeArray> bufferPair : queryBuffers) {
+      NativeArray offsets = bufferPair.getFirst();
+      NativeArray values = bufferPair.getSecond();
+      if (values != null) {
+        totalBufferSizes += values.getNBytes();
+        if (values.getNBytes() > largestSingleBuffer) {
+          largestSingleBuffer = values.getNBytes();
+        }
+        bufferCount++;
+      }
+      if (offsets != null) {
+        totalBufferSizes += offsets.getNBytes();
+        if (offsets.getNBytes() > largestSingleBuffer) {
+          largestSingleBuffer = offsets.getNBytes();
+        }
+      }
+      bufferCount++;
+    }
+    log.log(
+        Level.INFO,
+        "Largest single buffer is "
+            + largestSingleBuffer
+            + " total buffer count is "
+            + bufferCount);
+
+    return totalBufferSizes;
+  }
+
+  /**
+   * Check if we can double the buffer, or if there is not enough memory space
+   *
+   * @return
+   */
+  private boolean canReallocBuffers() {
+    long freeMemory = this.hardwareAbstractionLayer.getMemory().getAvailable();
+
+    long totalBufferSizes = calculateNativeArrayByteSizes();
+
+    log.log(
+        Level.INFO,
+        "Checking to realloc buffers from "
+            + totalBufferSizes
+            + " to "
+            + 2 * totalBufferSizes
+            + " with "
+            + freeMemory
+            + " memory free");
+
+    // If we are going to double the buffers we need to make sure we have 4x space for
+    // doubling the native buffer and copying to java arrays
+    return freeMemory > (4 * totalBufferSizes);
+  }
+
+  private void reallocateQueryBuffers() throws TileDBError {
+    if (!canReallocBuffers()) {
+      throw new TileDBError("Not enough memory to complete query!");
+    }
+
+    if (resultBatch != null) {
+      resultBatch.close();
+    }
+
+    query.resetBuffers();
+
+    this.read_query_buffer_size *= 2;
+
+    allocateQuerybuffers(this.read_query_buffer_size);
+  }
+
+  private void allocateQuerybuffers(long readBufferSize) throws TileDBError {
+
+    try (Domain domain = arraySchema.getDomain(); ) {
+      // Create coordinate buffers
+      int ncoords = Math.toIntExact(readBufferSize / domain.getType().getNativeSize());
+      NativeArray coordBuffer = new NativeArray(ctx, ncoords, domain.getType());
+
+      // loop over all attributes and set the query buffers based on buffer size
       // the query object handles the lifetime of the allocated (offheap) NativeArrays
+      int i = 0;
       for (StructField field : sparkSchema.fields()) {
         // get the spark column name and match to array schema
         String name = field.name();
         if (domain.hasDimension(name)) {
+          queryBuffers.set(i++, new Pair<>(null, coordBuffer));
           // dimension column (coordinate buffer allocation handled at the end)
           continue;
         }
         try (Attribute attr = arraySchema.getAttribute(name)) {
+          int nvalues = Math.toIntExact(readBufferSize / attr.getType().getNativeSize());
+          NativeArray data = new NativeArray(ctx, nvalues, attr.getType());
           // attribute is variable length, init the varlen result buffers using the est num offsets
           if (attr.isVar()) {
-            int noffsets =
-                Math.toIntExact(this.read_query_buffer_size / TILEDB_UINT64.getNativeSize());
-            int nvalues =
-                Math.toIntExact(this.read_query_buffer_size / attr.getType().getNativeSize());
-            query.setBuffer(
-                name,
-                new NativeArray(ctx, noffsets, TILEDB_UINT64),
-                new NativeArray(ctx, nvalues, attr.getType()));
+            int noffsets = Math.toIntExact(readBufferSize / TILEDB_UINT64.getNativeSize());
+            NativeArray offsets = new NativeArray(ctx, noffsets, TILEDB_UINT64);
+            query.setBuffer(name, offsets, data);
+            queryBuffers.set(i++, new Pair<>(offsets, data));
           } else {
             // attribute is fixed length, use the result size estimate for allocation
-            int nvalues =
-                Math.toIntExact(this.read_query_buffer_size / attr.getType().getNativeSize());
             query.setBuffer(name, new NativeArray(ctx, nvalues, attr.getType()));
+            queryBuffers.set(i++, new Pair<>(null, data));
           }
         }
       }
       // set the coordinate buffer result buffer
-      int ncoords = Math.toIntExact(this.read_query_buffer_size / domain.getType().getNativeSize());
-      query.setCoordinates(new NativeArray(ctx, ncoords, domain.getType()));
+      query.setCoordinates(coordBuffer);
 
       // Allocate result set batch based on the estimated (upper bound) number of rows / cells
       resultVectors =
@@ -215,8 +362,6 @@ public class TileDBDataReaderPartitionScan implements InputPartitionReader<Colum
               Math.toIntExact(ncoords / domain.getNDim()), sparkSchema);
       resultBatch = new ColumnarBatch(resultVectors);
     }
-    // est that there are resuts, so perform a read for this partition
-    return true;
   }
 
   /**
@@ -682,5 +827,22 @@ public class TileDBDataReaderPartitionScan implements InputPartitionReader<Colum
       }
     }
     return numValues;
+  }
+
+  /** Close out all the NativeArray objects */
+  private void closeQueryNativeArrays() {
+    for (Pair<NativeArray, NativeArray> bufferSet : queryBuffers) {
+      if (bufferSet == null) {
+        continue;
+      }
+      NativeArray offsetArray = bufferSet.getFirst();
+      NativeArray valuesArray = bufferSet.getSecond();
+      if (offsetArray != null) {
+        offsetArray.close();
+      }
+      if (valuesArray != null) {
+        valuesArray.close();
+      }
+    }
   }
 }
