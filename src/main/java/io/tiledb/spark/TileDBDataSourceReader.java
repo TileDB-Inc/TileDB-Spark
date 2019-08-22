35693d849a147a3aa46e4feaea44022ff14d40a8
Seth Shelnutt <seth@tiledb.io>
2019-08-22 14:58:33 -0400

    Reorder filter eval to before parition creation
    This lays the foundation for partitioning

--- src/main/java/io/tiledb/spark/TileDBDataSourceReader.java
+++ src/main/java/io/tiledb/spark/TileDBDataSourceReader.java
@@ -1,7 +1,15 @@
 package io.tiledb.spark;
 
+import static org.apache.log4j.Priority.ERROR;
+
+import io.tiledb.java.api.Array;
+import io.tiledb.java.api.Context;
+import io.tiledb.java.api.Datatype;
+import io.tiledb.java.api.Pair;
+import io.tiledb.java.api.TileDBError;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.sources.EqualNullSafe;
@@ -120,10 +128,164 @@ public class TileDBDataSourceReader
   @Override
   public List<InputPartition<ColumnarBatch>> planBatchInputPartitions() {
     ArrayList<InputPartition<ColumnarBatch>> readerPartitions = new ArrayList<>();
-    // TODO: multiple subarray partitioning
-    readerPartitions.add(
-        new TileDBDataReaderPartition(uri, tileDBReadSchema, tiledbOptions, pushedFilters));
+
+    try (Context ctx = new Context(tiledbOptions.getTileDBConfigMap());
+        // fetch and load the array to get nonEmptyDomain
+        Array array = new Array(ctx, uri.toString()); ) {
+      HashMap<String, Pair> nonEmptyDomain = array.nonEmptyDomain();
+      List<List<Pair>> ranges = new ArrayList<>();
+      for (Filter filter : pushedFilters) {
+        List<List<Pair>> dimRanges =
+            buildRangeFromFilter(filter, this.tileDBReadSchema.domainType, nonEmptyDomain);
+
+        for (int i = 0; i < dimRanges.size(); i++) {
+          while (ranges.size() < i + 1) {
+            ranges.add(new ArrayList<>());
+          }
+          ranges.get(i).addAll(dimRanges.get(i));
+        }
+      }
+
+      // TODO: multiple subarray partitioning
+      readerPartitions.add(
+          new TileDBDataReaderPartition(uri, tileDBReadSchema, tiledbOptions, ranges));
+    } catch (TileDBError tileDBError) {
+      log.log(ERROR, tileDBError.getMessage());
+      return readerPartitions;
+    }
     // foo
     return readerPartitions;
+  }
+
+  /**
+   * Creates range(s) from a filter that has been pushed down
+   *
+   * @param filter
+   * @param domainType
+   * @param nonEmptyDomain
+   * @throws TileDBError
+   */
+  private List<List<Pair>> buildRangeFromFilter(
+      Filter filter, Datatype domainType, HashMap<String, Pair> nonEmptyDomain) throws TileDBError {
+    // Map<String, Integer> dimensionIndexing = new HashMap<>();
+    List<List<Pair>> ranges = new ArrayList<>();
+    // Build mapping for dimension name to index
+    for (int i = 0; i < this.tileDBReadSchema.dimensionIndexes.size(); i++) {
+      ranges.add(new ArrayList<>());
+    }
+    // First handle filter that are equal so `dim = 1`
+    if (filter instanceof EqualNullSafe) {
+      EqualNullSafe f = (EqualNullSafe) filter;
+      int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+      ranges.get(dimIndex).add(new Pair<>(f.value(), f.value()));
+    } else if (filter instanceof EqualTo) {
+      EqualTo f = (EqualTo) filter;
+      int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+      ranges.get(dimIndex).add(new Pair<>(f.value(), f.value()));
+
+      // GreaterThan is ranges which are in the form of `dim > 1`
+    } else if (filter instanceof GreaterThan) {
+      GreaterThan f = (GreaterThan) filter;
+      int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+      ranges
+          .get(dimIndex)
+          .add(
+              new Pair<>(
+                  addEpsilon(f.value(), domainType),
+                  nonEmptyDomain.get(f.attribute()).getSecond()));
+      // GreaterThanOrEqual is ranges which are in the form of `dim >= 1`
+    } else if (filter instanceof GreaterThanOrEqual) {
+      GreaterThanOrEqual f = (GreaterThanOrEqual) filter;
+      int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+      ranges
+          .get(dimIndex)
+          .add(new Pair<>(f.value(), nonEmptyDomain.get(f.attribute()).getSecond()));
+
+      // For in filters we will add every value as ranges of 1. `dim IN (1, 2, 3)`
+    } else if (filter instanceof In) {
+      In f = (In) filter;
+      // Add every value as a new range, TileDB will collapse into super ranges for us
+      for (Object value : f.values()) {
+        int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+        ranges.get(dimIndex).add(new Pair<>(value, value));
+      }
+
+      // LessThanl is ranges which are in the form of `dim < 1`
+    } else if (filter instanceof LessThan) {
+      LessThan f = (LessThan) filter;
+      int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+      ranges
+          .get(dimIndex)
+          .add(
+              new Pair<>(
+                  nonEmptyDomain.get(f.attribute()).getFirst(),
+                  subtractEpsilon(f.value(), domainType)));
+      // LessThanOrEqual is ranges which are in the form of `dim <= 1`
+    } else if (filter instanceof LessThanOrEqual) {
+      LessThanOrEqual f = (LessThanOrEqual) filter;
+      int dimIndex = this.tileDBReadSchema.dimensionIndexes.get(f.attribute());
+      ranges.get(dimIndex).add(new Pair<>(nonEmptyDomain.get(f.attribute()).getFirst(), f.value()));
+    } else {
+      throw new TileDBError("Unsupported filter type");
+    }
+    return ranges;
+  }
+
+  /** Returns v + eps, where eps is the smallest value for the datatype such that v + eps > v. */
+  private static Object addEpsilon(Object value, Datatype type) throws TileDBError {
+    switch (type) {
+      case TILEDB_CHAR:
+      case TILEDB_INT8:
+        return ((byte) value) < Byte.MAX_VALUE ? ((byte) value + 1) : value;
+      case TILEDB_INT16:
+        return ((short) value) < Short.MAX_VALUE ? ((short) value + 1) : value;
+      case TILEDB_INT32:
+        return ((int) value) < Integer.MAX_VALUE ? ((int) value + 1) : value;
+      case TILEDB_INT64:
+        return ((long) value) < Long.MAX_VALUE ? ((long) value + 1) : value;
+      case TILEDB_UINT8:
+        return ((short) value) < ((short) Byte.MAX_VALUE + 1) ? ((short) value + 1) : value;
+      case TILEDB_UINT16:
+        return ((int) value) < ((int) Short.MAX_VALUE + 1) ? ((int) value + 1) : value;
+      case TILEDB_UINT32:
+        return ((long) value) < ((long) Integer.MAX_VALUE + 1) ? ((long) value + 1) : value;
+      case TILEDB_UINT64:
+        return ((long) value) < ((long) Integer.MAX_VALUE + 1) ? ((long) value + 1) : value;
+      case TILEDB_FLOAT32:
+        return ((float) value) < Float.MAX_VALUE ? Math.nextUp((float) value) : value;
+      case TILEDB_FLOAT64:
+        return ((double) value) < Double.MAX_VALUE ? Math.nextUp((double) value) : value;
+      default:
+        throw new TileDBError("Unsupported TileDB Datatype enum: " + type);
+    }
+  }
+
+  /** Returns v - eps, where eps is the smallest value for the datatype such that v - eps < v. */
+  private static Object subtractEpsilon(Object value, Datatype type) throws TileDBError {
+    switch (type) {
+      case TILEDB_CHAR:
+      case TILEDB_INT8:
+        return ((byte) value) > Byte.MIN_VALUE ? ((byte) value - 1) : value;
+      case TILEDB_INT16:
+        return ((short) value) > Short.MIN_VALUE ? ((short) value - 1) : value;
+      case TILEDB_INT32:
+        return ((int) value) > Integer.MIN_VALUE ? ((int) value - 1) : value;
+      case TILEDB_INT64:
+        return ((long) value) > Long.MIN_VALUE ? ((long) value - 1) : value;
+      case TILEDB_UINT8:
+        return ((short) value) > ((short) Byte.MIN_VALUE - 1) ? ((short) value - 1) : value;
+      case TILEDB_UINT16:
+        return ((int) value) > ((int) Short.MIN_VALUE - 1) ? ((int) value - 1) : value;
+      case TILEDB_UINT32:
+        return ((long) value) > ((long) Integer.MIN_VALUE - 1) ? ((long) value - 1) : value;
+      case TILEDB_UINT64:
+        return ((long) value) > ((long) Integer.MIN_VALUE - 1) ? ((long) value - 1) : value;
+      case TILEDB_FLOAT32:
+        return ((float) value) > Float.MIN_VALUE ? Math.nextDown((float) value) : value;
+      case TILEDB_FLOAT64:
+        return ((double) value) > Double.MIN_VALUE ? Math.nextDown((double) value) : value;
+      default:
+        throw new TileDBError("Unsupported TileDB Datatype enum: " + type);
+    }
   }
 }
