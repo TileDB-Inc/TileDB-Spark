f4a2862061241c1e86d85200d816a3c9b20a60d6
Seth Shelnutt <seth@tiledb.io>
2019-09-04 15:39:36 -0400

    Use sparkEnv to get metric sources if task is null

--- src/main/java/org/apache/spark/metrics/MetricsUpdater.java
+++ src/main/java/org/apache/spark/metrics/MetricsUpdater.java
@@ -1,9 +1,11 @@
 package org.apache.spark.metrics;
 
 import java.util.Optional;
+import org.apache.spark.SparkEnv;
 import org.apache.spark.TaskContext;
 import org.apache.spark.metrics.source.Source;
 import scala.Option;
+import scala.collection.Seq;
 
 public abstract class MetricsUpdater implements MetricsUpdate {
   private String sourceName = "tiledb";
@@ -14,6 +16,10 @@ public abstract class MetricsUpdater implements MetricsUpdate {
       if (source.isDefined()) {
         return Optional.of((TileDBMetricsSource) source.get());
       }
+    } else {
+      SparkEnv env = SparkEnv.get();
+      Seq<Source> sources = env.metricsSystem().getSourcesByName(sourceName);
+      if (sources.length() > 0) return Optional.of((TileDBMetricsSource) sources.head());
     }
     return Optional.empty();
   }
