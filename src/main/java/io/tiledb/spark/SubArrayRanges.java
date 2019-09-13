d9db4802a811a6a26aee6ad5cdbf03e2a8a5d1c5
Seth Shelnutt <seth@tiledb.io>
2019-09-13 11:12:10 -0400

    Fix computation of splits for a given bucket size

--- src/main/java/io/tiledb/spark/SubArrayRanges.java
+++ src/main/java/io/tiledb/spark/SubArrayRanges.java
@@ -1,7 +1,6 @@
 package io.tiledb.spark;
 
 import static io.tiledb.spark.util.generateAllSubarrays;
-import static java.lang.Math.ceil;
 
 import io.tiledb.java.api.TileDBError;
 import java.util.ArrayList;
@@ -167,13 +166,13 @@ public class SubArrayRanges implements Comparable<SubArrayRanges> {
     return true;
   }
 
-  public List<SubArrayRanges> split(int splits) throws TileDBError {
+  public List<SubArrayRanges> split(int buckets) throws TileDBError {
     List<SubArrayRanges> newSubarrays = new ArrayList<>();
 
     // Handle base case where there is a single dimension
     if (ranges.size() == 1) {
       if (ranges.get(0).splittable()) {
-        for (Range newRange : ranges.get(0).splitRange(splits)) {
+        for (Range newRange : ranges.get(0).splitRange(buckets)) {
           List<Range> dimRanges = new ArrayList<>();
           dimRanges.add(newRange);
           newSubarrays.add(new SubArrayRanges(dimRanges, datatype));
@@ -181,24 +180,70 @@ public class SubArrayRanges implements Comparable<SubArrayRanges> {
       }
     } else {
 
+      /*
+       If there is more than one dimension we need to find the maximum number of splits which
+       we can use in total to get the total number of buckets we want. This is found by first
+       taking the Nth root of the buckets where N is the number of dimensions. This yields the
+       average number of splits we need per partition to yield the maximum number of buckets
+       that is equal to or less than the requested number of buckets.
+
+       For example if the user requested 28 buckets, and there is 4 dimensions we do 28 ** (1/4) == 2.3003.
+       This number 2.3003 is then multipled by the number of dimension (4) and rounded down for Math.floor(2.303 * 4) = 9
+       We have a total of 9 splits that can be distributed among all dimensions, because the maximum number of
+       buckets that can be created is 2*2*2*3 = 24.
+      */
+      int totalAvailableSplits =
+          (int) Math.floor(Math.pow(buckets, 1d / ranges.size()) * ranges.size());
+
+      // Compute the volume ratios
       List<Double> dimensionVolumeRatios = computeDimensionVolumeRations();
+
+      /*
+       The weights are computed based on the width of each range
+       For example if we have 3 ranges (dimensions) with width 10, 15 and 35
+       the percentages are previously computed to be:
+       10 / (10+15+35) = 0.16666, 15/(10+15+35) = 0.25, 35 / (10+15+35) = 0.58333
+       We take the total number of splits available as computed above and multiply by the percentage
+       If there is 6 splits, they are computed to be:
+       6*0.1666 / 3 = 1, 6*0.25/3 = 1 and 6*0.5833/3 = 2
+      */
+      List<Integer> dimensionSplits =
+          dimensionVolumeRatios
+              .stream()
+              .map(e -> (int) Math.floor(totalAvailableSplits * e))
+              .collect(Collectors.toList());
+
+      // Find any left over splits available. Since we floor the splits per dimensions there might
+      // be splits leftover
+      int leftOvers = totalAvailableSplits - dimensionSplits.stream().reduce(0, Integer::sum);
+
+      // Get the sorted dimension by weight so we can apply any left over splits to the larger
+      // dimensions
+      int[] sortedIndices =
+          IntStream.range(0, dimensionVolumeRatios.size())
+              .boxed()
+              .sorted(
+                  (i, j) -> dimensionVolumeRatios.get(i).compareTo(dimensionVolumeRatios.get(j)))
+              .mapToInt(ele -> ele)
+              .toArray();
+
+      // Apply left over splits
+      while (leftOvers > 0) {
+        for (int dimIndex : sortedIndices) {
+          dimensionSplits.set(dimIndex, dimensionSplits.get(dimIndex) + 1);
+          leftOvers--;
+          if (leftOvers <= 0) {
+            break;
+          }
+        }
+      }
 
       List<List<Range>> newSplits = new ArrayList<>();
       // Use volume ratios to weightily determine splits
       for (int i = 0; i < ranges.size(); i++) {
         if (ranges.get(i).splittable()) {
-
-          /*
-           The weights are computed based on the width of each range
-           For example if we have 3 ranges (dimensions) with width 10, 15 and 35
-           the percentages are previously computed to be:
-           10 / (10+15+35) = 0.16666, 15/(10+15+35) = 0.25, 35 / (10+15+35) = 0.58333
-           We take the total number of splits multiply by the percentage and divide by the number of ranges (dimensions)
-           If there is 6 splits, they are computed to be:
-           6*0.1666 / 3 = 1, 6*0.25/3 = 1 and 6*0.5833/3 = 2
-          */
-          int dimensionWeightedSplits =
-              (int) ceil(((splits * dimensionVolumeRatios.get(i)) / ranges.size()));
+          // Get the weights computed above
+          int dimensionWeightedSplits = dimensionSplits.get(i);
 
           // Split the given range for a dimension into x splits
           newSplits.add(new ArrayList<>(ranges.get(i).splitRange(dimensionWeightedSplits)));
