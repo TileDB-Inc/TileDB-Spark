3b75eb25aaf751c332a372294f896902b0cd6453
gsvic <victorasgs@gmail.com>
2020-01-09 21:02:15 +0200

    Integrate JavaArray from TileDB-Java 0.2.3

--- scripts/emr-bootstrap/install-tiledb-spark.sh
+++ scripts/emr-bootstrap/install-tiledb-spark.sh
@@ -2,7 +2,7 @@
 
 sudo yum install -y cmake3 gcc g++ git &&
 sudo ln -s /usr/bin/cmake3 /usr/bin/cmake &&
-git clone https://github.com/TileDB-Inc/TileDB-Java.git -b 0.2.2 &&
+git clone https://github.com/TileDB-Inc/TileDB-Java.git -b 0.2.3 &&
 cd TileDB-Java &&
 ./gradlew -PTILEDB_S3=ON -PTILEDB_VERBOSE=ON assemble &&
 ./gradlew -PTILEDB_S3=ON -PTILEDB_VERBOSE=ON publishToMavenLocal &&
@@ -13,5 +13,5 @@ cd TileDB-Spark &&
 ./gradlew assemble &&
 ./gradlew shadowJar &&
 sudo mkdir -p /usr/lib/spark/jars &&
-sudo cp -r build/libs/tiledb-spark-metrics-0.0.4.jar /usr/lib/spark/jars &&
+sudo cp -r build/libs/tiledb-spark-metrics-0.0.5.jar /usr/lib/spark/jars &&
 cd ../
