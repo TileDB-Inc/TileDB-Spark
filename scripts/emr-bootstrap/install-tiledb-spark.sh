a00742afa57300526cb2f755c4021c9b7bb21215
gsvic <victorasgs@gmail.com>
2020-01-13 21:40:48 +0200

    updated to Spark 2.4.4

--- scripts/emr-bootstrap/install-tiledb-spark.sh
+++ scripts/emr-bootstrap/install-tiledb-spark.sh
@@ -3,4 +3,4 @@
 curl -s https://api.github.com/repos/TileDB-Inc/TileDB-Spark/releases/latest | grep browser_download_url | cut -d '"' -f 4 | xargs wget -qi -
 
 sudo mkdir -p /usr/lib/spark/jars &&
-sudo cp -r tiledb-spark-metrics-*.jar /usr/lib/spark/jars
+sudo cp -r tiledb-spark*.jar /usr/lib/spark/jars
